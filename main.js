1b15b2e55ad15bb669551727f7267d32af9ccd5f
Cynthia Ndiramiye <98228087+gitarecy@users.noreply.github.com>
2023-07-21 19:27:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ window.addEventListener('load', () => {
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
 		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
+		//const task_edit_el = document.createElement('button');
+		//task_edit_el.classList.add('edit');
+		//task_edit_el.innerText = 'Edit';
 
 		const task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
